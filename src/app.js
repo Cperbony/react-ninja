ebdece832277d2089b13242b0b6e77f3a684664d
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-06-24 22:31:15 -0300

    Iniciando com Formulários

--- src/app.js
+++ src/app.js
@@ -5,13 +5,31 @@ import Button from './button'
 
 // Criar classes, extendendo componente React
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      value: 'Valor Inicial'
+    }
+  }
+
   render() {
     return (
       <div>
-        <Button handleClick={() => (console.log('clicou!'))}>
-          Clique em Mim!
-          </Button>
+        <form>
+          <input type='text' value={this.state.value} onChange={(e) => {
+            console.log(e.nativeEvent)
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form>
       </div>
+      // <div>
+      //   <Button handleClick={() => (console.log('clicou!'))}>
+      //     Clique em Mim!
+      //     </Button>
+      // </div>
     )
   }
 }
