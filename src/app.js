b505d97b0e53e08d38ee1657e0d733395ee81ecb
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-06-25 22:35:15 -0300

    Formularios - Checkbox e Radios

--- src/app.js
+++ src/app.js
@@ -9,27 +9,36 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'Valor Inicial'
+      value: 'Valor Inicial',
+      checked: false
     }
   }
 
   render() {
     return (
-      <div>
-        <form>
-          <input type='text' value={this.state.value} onChange={(e) => {
-            console.log(e.nativeEvent)
-            this.setState({
-              value: e.target.value
-            })
-          }} />
-        </form>
-      </div>
-      // <div>
-      //   <Button handleClick={() => (console.log('clicou!'))}>
-      //     Clique em Mim!
-      //     </Button>
-      // </div>
+      <form>
+        <input type='text' value={this.state.value} onChange={(e) => {
+          console.log(e.nativeEvent)
+          this.setState({
+            value: e.target.value
+          })
+        }} />
+
+        <label>
+          <input type='checkbox'
+            value='my-checkbox'
+            defaultChecked
+          // checked={this.state.checked}
+          // onChange={(e) => {
+          //   this.setState({ checked: e.target.checked })
+          // }}
+          />
+          Checkbox
+        </label>
+
+        <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
+        <input type='radio' name='rd' value='2' /> Radio 2
+      </form>
     )
   }
 }
