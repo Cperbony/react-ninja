90e25320be361f361bad4677e941794fa42ed2e3
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-07-22 12:36:32 -0300

    TDD - some e reverse

---
+++ jest-in-pratice/src/some/some-recursive.test.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import { expect } from 'chai'
+import some from './some-recursive'
+
+it('some should be a function', () => {
+  expect(some).to.be.a('function')
+})
+
+it('some([], (item) => item) should return true', () => {
+  expect(some([], (item) => item)).to.be.ok
+})
+
+it('some[0, 2, 3], (item) => (item) should return false', () => {
+  expect(some([0, 2, 3], (item) => item)).to.not.be.ok
+})
+
+it('some([1, 2, 3], (item) => (item) should return true', () => {
+  expect(some([1, 2, 3], (item) => item)).to.be.ok
+})
+
+it('some[1, 2, 3], (item, index) => index === item -1) should return true', () => {
+  expect(some([1, 2, 3], (item, index) => index === item - 1)).to.be.ok
+})
+
+it('some[1, 2, 3], (item, index, array) => array.length === 3) should return true', () => {
+  expect(some([1, 2, 3], (item, index, array) => array.length === 3)).to.be.ok
+})
